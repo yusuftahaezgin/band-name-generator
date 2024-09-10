3f00584b94f6dba7a1508709dd20abc4f446a5ed
Taha Ezgin <112575944+yusuftahaezgin@users.noreply.github.com>
2024-09-10 16:05:01 +0300

    Update main.py

--- blind-auction/main.py
+++ blind-auction/main.py
@@ -1,31 +1,25 @@
-from replit import clear
-from art import logo
-#HINT: You can call clear() to clear the output in the console.
-bids={}
-
-def find_high_bid(bidders): # en yuksek teklifi bulan fonksiyon
+import art
+print(art.logo)
+bids = {}
+def find_high_bid():
     max_bid=0
     winner_person=""
-    for person in bidders:
-        if bidders[person]>max_bid:
-            max_bid=bidders[person]
+    for person in bids:
+        if max_bid < bids[person]:
+            max_bid =bids[person]
             winner_person=person
-    print(f"The winner is {winner_person} with a bid of ${max_bid}")
 
+    print(f"Max bid is ${max_bid} and winner is {winner_person}")
 
-print(logo)
-print("Welcome to the secret auction program.")
-
-is_Finish=True
-while is_Finish==True:
-    name= input("What is your name?: ")
-    bid= int(input("What's your bid?: $"))
-    bids[name]= bid
-    another_offer=input("Are there any other bidders? Type 'yes' or 'no'\n").lower()
+is_Finish=False
+while is_Finish!=True:
+    name=input("What is your name?: ")
+    bid=input("What is your bid?: $")
+    bids[name] =int(bid)
+    another_offer=input("Are there any other bidders? Type 'yes' or 'no'").lower()
     if another_offer=="yes":
-        clear()
-    else:
-        is_Finish=False
-        clear()
-        find_high_bid(bids)
+        print("\n"*300)
+    elif another_offer=="no":
+        is_Finish= True
+        find_high_bid()
 
